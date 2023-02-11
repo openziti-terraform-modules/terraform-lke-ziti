218293ea7fdef008f4ec29468cfae82f457cad54
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-02-10 23:25:07 -0500

    checkpoint

--- lke/main.tf
+++ lke/main.tf
@@ -67,6 +67,7 @@ provider "kubectl" {     # duplcates config of provider "kubernetes" for cert-ma
   host                   = yamldecode(base64decode(linode_lke_cluster.linode_lke.kubeconfig)).clusters[0].cluster.server
   token                  = yamldecode(base64decode(linode_lke_cluster.linode_lke.kubeconfig)).users[0].user.token
   cluster_ca_certificate = base64decode(yamldecode(base64decode(linode_lke_cluster.linode_lke.kubeconfig)).clusters[0].cluster.certificate-authority-data)
+  load_config_file       = false
 }
 
 module "cert_manager" {
@@ -75,6 +76,18 @@ module "cert_manager" {
   cluster_issuer_email                   = var.email
   cluster_issuer_name                    = "cert-manager-global"
   cluster_issuer_private_key_secret_name = "cert-manager-global-secret"
+  additional_set = [{
+    name = "enableCertificateOwnerRef"
+    value = "true"
+  }]
+}
+
+resource "helm_release" "trust_manager" {
+  depends_on   = [module.cert_manager]
+  chart      = "trust-manager"
+  repository = "https://charts.jetstack.io"
+  name       = "trust-manager"
+  namespace  = module.cert_manager.namespace
 }
 
 resource "helm_release" "ingress-nginx" {
@@ -87,7 +100,23 @@ resource "helm_release" "ingress-nginx" {
 resource "helm_release" "ziti-console" {
   depends_on = [helm_release.ingress-nginx]
   name = "ziti-console-release"
-  chart = "../../netfoundry/github/openziti-helm-charts/charts/ziti-console/"
+  chart = "./charts/ziti-console"
   values = ["${file("ziti-console-release-values.yaml")}"]
+}
+
+resource "helm_release" "ziti-controller" {
+  depends_on = [helm_release.trust_manager]
+  namespace = "ziti-controller"
+  create_namespace = true
+  name = "ziti-controller-release"
+  chart = "./charts/ziti-controller"
+  set {
+    name = "advertisedHost"
+    value = "ziti.lke.bingnet.cloud"
+  }
+  set {
+    name = "persistence.storageClass"
+    value = "linode-block-storage"
+  }
 }
 
