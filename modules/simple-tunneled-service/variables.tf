8738d84f36471a01d05e425d284832c34d8f0f96
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-03-13 23:16:10 -0400

    express helm values and restapi objects as hcl

--- modules/simple-tunneled-service/variables.tf
+++ modules/simple-tunneled-service/variables.tf
@@ -1,18 +1,21 @@
 variable "intercept_config_type_id" {
     description = "the ziti id of the intercept.v1 config type"
+    type = string
 }
 
 variable "host_config_type_id" {
     description = "the ziti id of the host.v1 config type"
+    type = string
 }
 
-variable "role_attribute" {
-    # type = list(string)  # TODO: iterate over members as JSON in template
+variable "role_attributes" {
     description = "service role to assign"
+    type = list
 }
 
 variable "upstream_address" {
     description = "server address that provides this service"
+    type = string
 }
 
 variable "upstream_port" {
@@ -22,6 +25,7 @@ variable "upstream_port" {
 
 variable "intercept_address" {
     description = "advertised address used by consumers to reach this service"
+    type = string
 }
 
 variable "intercept_port" {
@@ -32,8 +36,34 @@ variable "intercept_port" {
 variable "transport_protocol" {
     description = "tcp | udp"
     default = "tcp"
+    type = string
 }
 
 variable "name" {
     description = "name slug to uniquely identify the several resources created for this service"
+    type = string
+}
+
+variable "bind_semantic" {
+    description = "bind policy semantic to apply to identity and service roles"
+    default = "AnyOf"
+    type = string
+}
+
+variable "dial_semantic" {
+    description = "dial policy semantic to apply to identity and service roles"
+    default = "AnyOf"
+    type = string
+}
+
+variable "bind_identity_roles" {
+    description = "additional identity roles that may bind (host) this service, default is '{name}-hosts'"
+    default = []
+    type = list
+}
+
+variable "dial_identity_roles" {
+    description = "identity roles that may dial (access) this service, default is '{name}-clients'"
+    default = []
+    type = list
 }
--- plan-30-services/main.tf
+++ plan-30-services/main.tf
@@ -32,8 +32,8 @@ provider restapi {
 }
 
 provider "helm" {
-    repository_config_path = "${path.root}/.helm/repositories.yaml" 
-    repository_cache       = "${path.root}/.helm"
+    repository_config_path     = "${path.root}/.helm/repositories.yaml" 
+    repository_cache           = "${path.root}/.helm"
     kubernetes {
         host                   = yamldecode(base64decode(data.terraform_remote_state.lke_state.outputs.kubeconfig)).clusters[0].cluster.server
         token                  = yamldecode(base64decode(data.terraform_remote_state.lke_state.outputs.kubeconfig)).users[0].user.token
@@ -51,95 +51,84 @@ data "restapi_object" "router1_identity_lookup" {
 }
 
 resource "restapi_object" "router1_identity" {
-    depends_on    = [data.restapi_object.router1_identity_lookup]
-    debug         = true
-    provider      = restapi
-    path          = "/identities"
-    update_method = "PATCH"
-    data = <<-EOF
-        {
-            "id": "${jsondecode(data.restapi_object.router1_identity_lookup.api_response).data.id}",
-            "roleAttributes": [
-                "mgmt-hosts",
-                "k8sapi-hosts"
-            ]
-        }
-    EOF
+    depends_on         = [data.restapi_object.router1_identity_lookup]
+    provider           = restapi
+    path               = "/identities"
+    update_method      = "PATCH"
+    data               = jsonencode({
+        id             = jsondecode(data.restapi_object.router1_identity_lookup.api_response).data.id
+        roleAttributes = [
+            "mgmt-hosts",
+            "k8sapi-hosts"
+        ]
+    })
 }
 
 resource "restapi_object" "client_identity" {
-    provider    = restapi
-    path        = "/identities"
-    data = <<-EOF
-        {
-            "name": "edge-client",
-            "type": "Device",
-            "isAdmin": false,
-            "enrollment": {
-                "ott": true
-            },
-            "roleAttributes": [
-                "webhook-clients",
-                "k8sapi-clients",
-                "mgmt-clients"
-            ]
+    debug              = true
+    provider           = restapi
+    path               = "/identities"
+    data               = jsonencode({
+        name           = "edge-client"
+        type           = "Device"
+        isAdmin        = false
+        enrollment     = {
+            ott        = true
         }
-    EOF
+        roleAttributes = [
+            "webhook-clients",
+            "k8sapi-clients",
+            "mgmt-clients"
+        ]
+    })
 }
 
 resource "local_file" "client_identity" {
     depends_on = [restapi_object.client_identity]
-    content  = try(jsondecode(restapi_object.client_identity.api_response).data.enrollment.ott.jwt, "dummystring")
-    # filename = "${path.root}/.terraform/tmp/edge-client.jwt"
-    filename = "/tmp/lke-edge-client.jwt"
+    content    = try(jsondecode(restapi_object.client_identity.api_response).data.enrollment.ott.jwt, "dummystring")
+    filename   = "/tmp/lke-edge-client.jwt"
 }
 
 module "mgmt_service" {
-    source = "../modules/simple-tunneled-service"
+    source                   = "../modules/simple-tunneled-service"
     intercept_config_type_id = jsondecode(data.restapi_object.intercept_v1_config_type.api_response).data.id
-    host_config_type_id = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
-    upstream_address = data.terraform_remote_state.lke_state.outputs.ziti_controller_mgmt_internal_host
-    upstream_port = 443
-    intercept_address = "mgmt.ziti"
-    intercept_port = 443
-    role_attribute = "mgmt-services"
-    name = "mgmt"
+    host_config_type_id      = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
+    upstream_address         = data.terraform_remote_state.lke_state.outputs.ziti_controller_mgmt_internal_host
+    upstream_port            = 443
+    intercept_address        = "mgmt.ziti"
+    intercept_port           = 443
+    role_attributes          = ["mgmt-services"]
+    name                     = "mgmt"
 }
 
 module "k8sapi_service" {
-    source = "../modules/simple-tunneled-service"
+    source                   = "../modules/simple-tunneled-service"
     intercept_config_type_id = jsondecode(data.restapi_object.intercept_v1_config_type.api_response).data.id
-    host_config_type_id = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
-    upstream_address = "kubernetes.default.svc"
-    upstream_port = 443
-    intercept_address = "kubernetes.default.svc"
-    intercept_port = 443
-    role_attribute = "k8sapi-services"
-    name = "k8sapi"
+    host_config_type_id      = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
+    upstream_address         = "kubernetes.default.svc"
+    upstream_port            = 443
+    intercept_address        = "kubernetes.default.svc"
+    intercept_port           = 443
+    role_attributes          = ["k8sapi-services"]
+    name                     = "k8sapi"
 }
 
 resource "restapi_object" "webhook_host_identity" {
-    provider    = restapi
-    path        = "/identities"
-    data = <<-EOF
-        {
-            "name": "webhook-host",
-            "type": "Device",
-            "isAdmin": false,
-            "enrollment": {
-                "ott": true
-            },
-            "roleAttributes": [
-                "webhook-hosts"
-            ]
+    provider           = restapi
+    path               = "/identities"
+    data               = jsonencode({
+        name           = "webhook-host"
+        type           = "Device"
+        isAdmin        = false
+        enrollment     = {
+            ott        = true
         }
-    EOF
+        roleAttributes = ["webhook-hosts"]
+    })
 }
 
 resource "null_resource" "enroll_webhook_host_identity" {
-    depends_on = [
-        restapi_object.webhook_host_identity
-    ]
+    depends_on = [restapi_object.webhook_host_identity]
     provisioner "local-exec" {
         command = <<-EOF
             ziti edge enroll \
@@ -152,24 +141,24 @@ resource "null_resource" "enroll_webhook_host_identity" {
 
 data "local_file" "webhook_host_identity" {
     depends_on = [null_resource.enroll_webhook_host_identity]
-    filename = "${path.root}/.terraform/webhook-host.json"
+    filename   = "${path.root}/.terraform/webhook-host.json"
 }
 
 resource "helm_release" "webhook_host" {
-    depends_on   = [null_resource.enroll_webhook_host_identity]
-    chart        = var.ziti_charts != "" ? "${var.ziti_charts}/httpbin" : "httpbin"
-    version      = ">=0.1.8"
-    repository   = "https://openziti.github.io/helm-charts"
-    name         = "webhook-host"
-    namespace    = "default"
-    set {
-        name = "zitiServiceName"
-        value = "webhook-service"
+    depends_on    = [null_resource.enroll_webhook_host_identity]
+    chart         = var.ziti_charts != "" ? "${var.ziti_charts}/httpbin" : "httpbin"
+    version       = ">=0.1.8"
+    repository    = "https://openziti.github.io/helm-charts"
+    name          = "webhook-host"
+    namespace     = "default"
+    set             {
+        name      = "zitiServiceName"
+        value     = "webhook-service"
     }
-    set_sensitive {
-        name = "zitiIdentityEncoding"
-        value = base64encode(data.local_file.webhook_host_identity.content)
-        type  = "auto"
+    set_sensitive   {
+        name      = "zitiIdentityEncoding"
+        value     = base64encode(data.local_file.webhook_host_identity.content)
+        type      = "auto"
     }
 }
 
@@ -190,18 +179,18 @@ data "restapi_object" "host_v1_config_type" {
 
 
 module "webhook_service" {
-    source = "../modules/simple-tunneled-service"
+    source                   = "../modules/simple-tunneled-service"
     intercept_config_type_id = jsondecode(data.restapi_object.intercept_v1_config_type.api_response).data.id
-    host_config_type_id = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
-    upstream_address = "httpbin.default.svc"
-    upstream_port = 8080
-    intercept_address = "webhook.ziti"
-    intercept_port = 80
-    role_attribute = "webhook-services"
-    name = "posthook"
+    host_config_type_id      = jsondecode(data.restapi_object.host_v1_config_type.api_response).data.id
+    upstream_address         = "httpbin.default.svc"
+    upstream_port            = 8080
+    intercept_address        = "webhook.ziti"
+    intercept_port           = 80
+    role_attributes          = ["webhook-services"]
+    name                     = "posthook"
 }
 
 module "public_routers" {
-    source = "../modules/public-router-policies"
-    router_role = "public-routers"
+    source       = "../modules/public-router-policies"
+    router_roles = ["#public-routers"]
 }
