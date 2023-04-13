9375285ca2b84f93e79b018a97646e53568c5840
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-04-13 00:49:54 -0400

    add vars for zrok email setup

--- plan-50-zrok/variables.tf
+++ plan-50-zrok/variables.tf
@@ -33,6 +33,38 @@ variable "storage_class" {
     description = "The storage class to use for persistent volumes"
     default     = ""
 }
+
+# the zrok chart will look for all values starting with "email_" if "email_host"
+# is set
+variable "email_host" {
+    type        = string
+    description = "The email host to use for sending emails"
+    default     = ""
+}
+
+variable "email_port" {
+    type        = number
+    description = "The email port to use for sending emails with TLS"
+    default     = 465
+}
+variable "email_from" {
+    type        = string
+    description = "The email address to use for the from field"
+    default     = ""
+}
+
+variable "email_username" {
+    type        = string
+    description = "The username to use for the email account"
+    default     = ""
+}
+
+variable "email_password" {
+    type        = string
+    description = "The password to use for the email account"
+    default     = ""
+}
+
 variable "zrok_controller_spec_version" {
     type        = number
     description = "The zrok controller spec version is '2' until zrok v0.4.0, and then it's '3'"
