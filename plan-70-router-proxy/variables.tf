34d30afbee506ec8ec24805af9f0cd59cce73a90
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-04-17 17:34:06 -0400

    add a module to demonstrate the router proxy

---
+++ plan-70-router-proxy/variables.tf
@@ -0,0 +1,24 @@
+variable "storage_class" {
+    type = string
+    default = ""
+}
+
+variable "ziti_charts" {
+    type = string
+    default = ""
+}
+
+variable "container_image_repo" {
+    description = "router container image repository"
+    default = "docker.io/openziti/ziti-router"
+}
+
+variable "container_image_tag" {
+    description = "router container image tag"
+    default = "latest"
+}
+
+variable "container_image_pull_policy" {
+    description = "router container image pull policy"
+    default = "IfNotPresent"
+}
