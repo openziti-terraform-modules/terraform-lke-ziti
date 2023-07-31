736f404ea7fb3920f798ddbfc3c3e859ab01856c
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-07-31 16:45:22 -0400

    reference tfe outputs instead of remote state data source

--- plan-15-ziti-controller/main.tf
+++ plan-15-ziti-controller/main.tf
@@ -37,30 +37,30 @@ provider "helm" {
   repository_config_path = "${path.root}/.helm/repositories.yaml"
   repository_cache       = "${path.root}/.helm"
   kubernetes {
-    host                   = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.server
-    token                  = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).users[0].user.token
-    cluster_ca_certificate = base64decode(yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.certificate-authority-data)
+    host                   = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.server
+    token                  = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).users[0].user.token
+    cluster_ca_certificate = base64decode(yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.certificate-authority-data)
   }
 }
 
 provider "kubernetes" {
-  host                   = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.server
-  token                  = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).users[0].user.token
-  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.certificate-authority-data)
+  host                   = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.server
+  token                  = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).users[0].user.token
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.certificate-authority-data)
 }
 
 provider "kubectl" { # duplcates config of provider "kubernetes" for cert-manager module
-  host                   = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.server
-  token                  = yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).users[0].user.token
-  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.terraform_remote_state.k8s_state.outputs.kubeconfig)).clusters[0].cluster.certificate-authority-data)
+  host                   = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.server
+  token                  = yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).users[0].user.token
+  cluster_ca_certificate = base64decode(yamldecode(base64decode(data.tfe_outputs.k8s_state.values.kubeconfig)).clusters[0].cluster.certificate-authority-data)
 }
 
 module "ziti_controller" {
   source                  = "github.com/openziti-test-kitchen/terraform-k8s-ziti-controller?ref=v0.1.1"
   ziti_charts             = var.ziti_charts
   ziti_controller_release = var.ziti_controller_release
-  ziti_namespace          = data.terraform_remote_state.k8s_state.outputs.ziti_namespace
-  dns_zone                = data.terraform_remote_state.k8s_state.outputs.dns_zone
+  ziti_namespace          = data.tfe_outputs.k8s_state.values.ziti_namespace
+  dns_zone                = data.tfe_outputs.k8s_state.values.dns_zone
   storage_class           = var.storage_class
   values = {
     image = {
@@ -87,7 +87,7 @@ module "ziti_controller" {
 
 resource "helm_release" "ziti_console" {
   name       = var.ziti_console_release
-  namespace  = data.terraform_remote_state.k8s_state.outputs.ziti_namespace
+  namespace  = data.tfe_outputs.k8s_state.values.ziti_namespace
   repository = "https://openziti.github.io/helm-charts"
   chart      = var.ziti_charts != "" ? "${var.ziti_charts}/ziti-console" : "ziti-console"
   version    = "~> 0.3"
@@ -96,15 +96,15 @@ resource "helm_release" "ziti_console" {
       enabled          = "true"
       ingressClassName = "nginx"
       annotations = {
-        "cert-manager.io/cluster-issuer" = data.terraform_remote_state.k8s_state.outputs.cluster_issuer_name
+        "cert-manager.io/cluster-issuer" = data.tfe_outputs.k8s_state.values.cluster_issuer_name
       }
-      advertisedHost = "console.${data.terraform_remote_state.k8s_state.outputs.dns_zone}"
+      advertisedHost = "console.${data.tfe_outputs.k8s_state.values.dns_zone}"
       tlsSecret      = "${var.ziti_console_release}-tls-secret"
     }
     settings = {
       edgeControllers = [{
         name    = "Ziti Edge Mgmt API"
-        url     = "https://${var.ziti_controller_release}-mgmt.${data.terraform_remote_state.k8s_state.outputs.ziti_namespace}.svc:443"
+        url     = "https://${var.ziti_controller_release}-mgmt.${data.tfe_outputs.k8s_state.values.ziti_namespace}.svc:443"
         default = "true"
       }]
     }
